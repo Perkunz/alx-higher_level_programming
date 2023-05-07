801f145c73015dd16b5648650420890ddd5c9923
Perkunz <ighokarop@gmail.com>
2023-05-07 14:58:26 -0700

    correction done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,11 +5,3 @@
 def lookup(obj):
     """Return a list of an object's available attributes."""
     return (dir(obj))
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-
